dffdb184c66632d22d29a7c421d5d240d2a8e175
Toby Hsieh <thrasher13b@gmail.com>
2023-06-11 23:45:00 -0700

    Create CommentUpserter class

---
+++ __tests__/comment-upserter.test.ts
@@ -0,0 +1,134 @@
+import {beforeEach, describe, it} from '@jest/globals'
+import {RestEndpointMethodTypes} from '@octokit/plugin-rest-endpoint-methods'
+import {RestEndpointMethods} from '@octokit/plugin-rest-endpoint-methods/dist-types/generated/method-types.d'
+import {EqualMatchingInjectorConfig, It, Mock, Times} from 'moq.ts'
+
+import CommentUpserter, {HEADER} from '../src/comment-upserter'
+import {Repo} from '../src/github-types'
+
+describe('CommentUpserter', () => {
+  let issuesMock: Mock<RestEndpointMethods['issues']>
+  let upserter: CommentUpserter
+
+  beforeEach(() => {
+    issuesMock = new Mock<RestEndpointMethods['issues']>({
+      injectorConfig: new EqualMatchingInjectorConfig()
+    })
+    const octokitRestMock = new Mock<RestEndpointMethods>()
+      .setup(instance => instance.issues)
+      .returns(issuesMock.object())
+    upserter = new CommentUpserter(octokitRestMock.object())
+  })
+
+  describe('.upsert', () => {
+    const repo: Repo = {owner: 'tobyhs', repo: 'codemention'}
+    const pullNumber = 17
+
+    describe('when there are no applicable mention rules', () => {
+      it('does not insert or update a comment', async () => {
+        await upserter.upsert(repo, pullNumber, [])
+        issuesMock.verify(
+          instance => instance.createComment(It.IsAny()),
+          Times.Never()
+        )
+        issuesMock.verify(
+          instance => instance.updateComment(It.IsAny()),
+          Times.Never()
+        )
+      })
+    })
+
+    const rules = [
+      {
+        patterns: ['db/migrate/**'],
+        mentions: ['cto', 'dba']
+      },
+      {
+        patterns: ['.github/**', 'spec/*.rb'],
+        mentions: ['ci']
+      }
+    ]
+
+    const commentBody =
+      HEADER +
+      [
+        '| db/migrate/** | @cto, @dba |',
+        '| .github/**<br>spec/*.rb | @ci |'
+      ].join('\n')
+
+    const stubListComments = (comments: string[]): void => {
+      const listResponse = {
+        data: comments.map((comment, index) => ({id: index + 1, body: comment}))
+      } as RestEndpointMethodTypes['issues']['listComments']['response']
+
+      issuesMock
+        .setup(instance =>
+          instance.listComments({...repo, issue_number: pullNumber})
+        )
+        .returnsAsync(listResponse)
+    }
+
+    describe('when a codemention comment does not exist', () => {
+      it('creates a comment', async () => {
+        stubListComments(['First', 'Second'])
+
+        issuesMock
+          .setup(instance => instance.createComment(It.IsAny()))
+          .returnsAsync(
+            new Mock<
+              RestEndpointMethodTypes['issues']['createComment']['response']
+            >().object()
+          )
+
+        await upserter.upsert(repo, pullNumber, rules)
+
+        issuesMock.verify(instance =>
+          instance.createComment({
+            ...repo,
+            issue_number: pullNumber,
+            body: commentBody
+          })
+        )
+      })
+    })
+
+    describe('when a codemention comment exists', () => {
+      describe('and the comment is different', () => {
+        it('updates the comment', async () => {
+          const existingComment = HEADER + '| config/brakeman.yml | @security |'
+          stubListComments(['First', existingComment])
+
+          issuesMock
+            .setup(instance => instance.updateComment(It.IsAny()))
+            .returnsAsync(
+              new Mock<
+                RestEndpointMethodTypes['issues']['updateComment']['response']
+              >().object()
+            )
+
+          await upserter.upsert(repo, pullNumber, rules)
+
+          issuesMock.verify(instance =>
+            instance.updateComment({
+              ...repo,
+              comment_id: 2,
+              body: commentBody
+            })
+          )
+        })
+      })
+
+      describe('and the comment is the same', () => {
+        it('does not update the comment', async () => {
+          stubListComments(['First', commentBody])
+          await upserter.upsert(repo, pullNumber, rules)
+
+          issuesMock.verify(
+            instance => instance.updateComment(It.IsAny()),
+            Times.Never()
+          )
+        })
+      })
+    })
+  })
+})
---
+++ src/comment-upserter.ts
@@ -0,0 +1,80 @@
+import {RestEndpointMethods} from '@octokit/plugin-rest-endpoint-methods/dist-types/generated/method-types.d'
+
+import {MentionRule} from './configuration'
+import {Repo} from './github-types'
+
+export const HEADER = [
+  '<!-- codemention header -->',
+  '[CodeMention](https://github.com/tobyhs/codemention):\n',
+  '| File Patterns | Mentions |',
+  '| - | - |\n'
+].join('\n')
+
+/**
+ * @see {@link upsert}
+ */
+export default class CommentUpserter {
+  /**
+   * @param octokitRest - GitHub REST API client
+   */
+  constructor(private readonly octokitRest: RestEndpointMethods) {}
+
+  /**
+   * Inserts or updates a pull request comment that mentions users/teams.
+   *
+   * @param repo - repository that the pull request is in
+   * @param pullNumber - number that identifies the pull request
+   * @param rules - mention rules to use in the comment
+   */
+  async upsert(
+    repo: Repo,
+    pullNumber: number,
+    rules: MentionRule[]
+  ): Promise<void> {
+    if (rules.length === 0) {
+      return
+    }
+
+    const issuesApi = this.octokitRest.issues
+    const listResponse = await issuesApi.listComments({
+      owner: repo.owner,
+      repo: repo.repo,
+      issue_number: pullNumber
+    })
+    const existingComment = listResponse.data.find(
+      c => c.body !== undefined && c.body.startsWith(HEADER)
+    )
+    const commentBody = this.createCommentBody(rules)
+
+    if (existingComment === undefined) {
+      await issuesApi.createComment({
+        owner: repo.owner,
+        repo: repo.repo,
+        issue_number: pullNumber,
+        body: commentBody
+      })
+    } else if (existingComment.body !== commentBody) {
+      await issuesApi.updateComment({
+        owner: repo.owner,
+        repo: repo.repo,
+        comment_id: existingComment.id,
+        body: commentBody
+      })
+    }
+  }
+
+  /**
+   * @param rules - mention rules to use in the comment
+   * @returns text to be used in a GitHub pull request comment body
+   */
+  private createCommentBody(rules: MentionRule[]): string {
+    const body = rules
+      .map(rule => {
+        const patterns = rule.patterns.join('<br>')
+        const mentions = rule.mentions.map(name => `@${name}`).join(', ')
+        return `| ${patterns} | ${mentions} |`
+      })
+      .join('\n')
+    return `${HEADER}${body}`
+  }
+}
