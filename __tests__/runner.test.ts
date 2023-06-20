7304535ef1fcfaf3238d3d61fd25c5eef69f6679
Toby Hsieh <thrasher13b@gmail.com>
2023-06-19 22:20:02 -0700

    Create Runner class

---
+++ __tests__/runner.test.ts
@@ -0,0 +1,91 @@
+import {Context} from '@actions/github/lib/context'
+import {beforeEach, describe, it} from '@jest/globals'
+import * as fs from 'fs'
+import * as yaml from 'js-yaml'
+import {EqualMatchingInjectorConfig, It, Mock} from 'moq.ts'
+import * as path from 'path'
+
+import {CommentUpserter} from '../src/comment-upserter'
+import {Configuration} from '../src/configuration'
+import {ConfigurationReader} from '../src/configuration-reader'
+import {FilesChangedReader} from '../src/files-changed-reader'
+import {Repo} from '../src/github-types'
+import Runner from '../src/runner'
+
+describe('Runner', () => {
+  let configurationReader: ConfigurationReader
+  let filesChangedReader: FilesChangedReader
+  let commentUpserterMock: Mock<CommentUpserter>
+  let runner: Runner
+
+  let repo: Repo
+  let context: Context
+  const prNumber = 123
+  const baseSha = 'bfc5b2d29cfa2db8ce40f6c60bc9629490fe1225'
+  const configuration: Configuration = yaml.load(
+    fs.readFileSync(path.join(__dirname, 'fixtures', 'codemention.yml'), 'utf8')
+  ) as Configuration
+
+  beforeEach(() => {
+    repo = {owner: 'tobyhs', repo: 'codemention'}
+    context = {
+      repo,
+      payload: {
+        pull_request: {
+          number: prNumber,
+          base: {sha: baseSha}
+        }
+      }
+    } as unknown as Context
+
+    configurationReader = new Mock<ConfigurationReader>({
+      injectorConfig: new EqualMatchingInjectorConfig()
+    })
+      .setup(async instance => instance.read(repo, baseSha))
+      .returnsAsync(configuration)
+      .object()
+
+    const filesChanged = [
+      'config/application.rb',
+      '.github/workflows/codemention.yml'
+    ]
+    filesChangedReader = new Mock<FilesChangedReader>({
+      injectorConfig: new EqualMatchingInjectorConfig()
+    })
+      .setup(async instance => instance.read(repo, prNumber))
+      .returnsAsync(filesChanged)
+      .object()
+
+    commentUpserterMock = new Mock<CommentUpserter>({
+      injectorConfig: new EqualMatchingInjectorConfig()
+    })
+    commentUpserterMock
+      .setup(instance => instance.upsert(It.IsAny(), It.IsAny(), It.IsAny()))
+      .returns(Promise.resolve())
+
+    runner = new Runner(
+      configurationReader,
+      filesChangedReader,
+      commentUpserterMock.object()
+    )
+  })
+
+  describe('.run', () => {
+    it('runs main logic of the GitHub action', async () => {
+      await runner.run(context)
+      const matchingRules = [
+        {
+          patterns: ['config/**'],
+          mentions: ['sysadmin']
+        },
+        {
+          patterns: ['.github/**', 'spec/*.rb'],
+          mentions: ['ci']
+        }
+      ]
+      commentUpserterMock.verify(instance =>
+        instance.upsert(repo, prNumber, matchingRules)
+      )
+    })
+  })
+})
---
+++ src/runner.ts
@@ -0,0 +1,47 @@
+import {Context} from '@actions/github/lib/context'
+import {PullRequestEvent} from '@octokit/webhooks-types/schema.d'
+import micromatch from 'micromatch'
+
+import {CommentUpserter} from './comment-upserter'
+import {ConfigurationReader} from './configuration-reader'
+import {FilesChangedReader} from './files-changed-reader'
+
+/**
+ * @see {@link run}
+ */
+export default class Runner {
+  /**
+   * @param configurationReader - object to read a repo's codemention.yml file
+   * @param filesChangedReader - object to retrieve files changed in a pull request
+   * @param commentUpserter - object to upsert pull request comments
+   */
+  constructor(
+    private readonly configurationReader: ConfigurationReader,
+    private readonly filesChangedReader: FilesChangedReader,
+    private readonly commentUpserter: CommentUpserter
+  ) {}
+
+  /**
+   * Runs the main logic of the GitHub action
+   *
+   * @param context - context of the GitHub action
+   */
+  async run(context: Context): Promise<void> {
+    const {repo} = context
+    const event = context.payload as PullRequestEvent
+    const pullRequest = event.pull_request
+
+    const configuration = await this.configurationReader.read(
+      repo,
+      pullRequest.base.sha
+    )
+    const filesChanged = await this.filesChangedReader.read(
+      repo,
+      pullRequest.number
+    )
+    const matchingRules = configuration.rules.filter(
+      rule => micromatch(filesChanged, rule.patterns).length > 0
+    )
+    await this.commentUpserter.upsert(repo, pullRequest.number, matchingRules)
+  }
+}
