ef762b2db8b98616bf82f5033f73fa432d509f6a
Toby Hsieh <thrasher13b@gmail.com>
2026-02-02 22:32:59 -0800

    Upgrade libraries that depend on undici
    This is to resolve a Dependabot alert for undici.

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
-module.exports = {
+export default {
   clearMocks: true,
   moduleFileExtensions: ['js', 'ts'],
   testMatch: ['**/*.test.ts'],
   transform: {
-    '^.+\\.ts$': 'ts-jest'
+    '^.+\\.[jt]s$': 'ts-jest',
   },
+  transformIgnorePatterns: ['/node_modules/(?!(@octokit|before-after-hook|universal-user-agent)/)'],
   verbose: true,
   collectCoverageFrom: ['src/**/*.ts'],
   coverageThreshold: {
